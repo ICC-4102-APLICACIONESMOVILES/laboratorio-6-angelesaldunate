3abb83d359bb6dc527a89af078fba2c83fa78319
angelesaldunate <31292145+angelesaldunate@users.noreply.github.com>
2018-03-27 00:25:25 -0300

    Lab 02 upgrade Navigation Drawer

--- app/src/main/java/com/example/angeles/labapp/MainActivity.java
+++ app/src/main/java/com/example/angeles/labapp/MainActivity.java
@@ -1,23 +1,38 @@
 package com.example.angeles.labapp;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     static final int SEND_MESSAGE = 1;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         CredentialManage nueva = new CredentialManage();
         if (!nueva.verificarCredenciales(this)) {
             Intent intent = new Intent(this, LoginActivity.class);
@@ -25,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
             //iniciaractividad solo si no existe anteriormente
             startActivityForResult(intent,SEND_MESSAGE);
         }
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
     public void logOut(View view) {
         CredentialManage nueva = new CredentialManage();
@@ -47,10 +64,10 @@ public class MainActivity extends AppCompatActivity {
 
                 String email = data.getStringExtra("email_devuelto");
                 String password = data.getStringExtra("password_devuelto");
-                TextView textViewmail = findViewById(R.id.textViewNombre);
-                textViewmail.setText(email);
-                TextView textViewpass = findViewById(R.id.textViewPassword);
-                textViewpass.setText(password);
+                //TextView textViewmail = findViewById(R.id.textViewNombre);
+               // textViewmail.setText(email);
+                //TextView textViewpass = findViewById(R.id.textViewPassword);
+                //textViewpass.setText(password);
 
                 CredentialManage nueva = new CredentialManage();
                 nueva.guardarCredenciales(this,email,password);
@@ -59,6 +76,52 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        Fragment fragment = null;
+        Boolean fragmentactive=false;
+
+        int id = item.getItemId();
+
+        if (id == R.id.db) {
+            fragmentactive=true;
+           // fragment=new AzulFragment();
+        } else if (id == R.id.search) {
+          //  fragment=new VerdeFragment();
+            fragmentactive=true;
+        //}// else if (id == R.id.nav_slideshow) {
+
+       // } //else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.logout) {
+            CredentialManage nueva = new CredentialManage();
+            nueva.borrarCredenciales(this);
+            Intent intent = new Intent(this, LoginActivity.class);
+            intent.putExtra(EXTRA_MESSAGE,"Sent!");
+            //iniciaractividad solo si no existe anteriormente
+            startActivityForResult(intent,SEND_MESSAGE);
+        }
+        if(fragmentactive){
+            getSupportFragmentManager().beginTransaction().replace(R.id.drawer_layout,fragment).commit();
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
 
